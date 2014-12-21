83e1eb30b32a89fdba6d134fcfe657d1d7cfb964
K.Pacharawat <K.Pacharawat@10.2.81.228>
2014-12-21 09:07:40 +0700

    File HelloWorld CPE4235

---
+++ src/Hello.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author K.Pacharawat
+ */
+public class Hello {
+    public static void main(String[] args){
+        System.out.print("HelloWorld!");
+        System.out.print("My Name is Pacharawat Kittipatvarong 5451700172");
+    }
+}
